00e5e13145244d173613ddf9e0623519a3bbe8f6
chaoism <1290574095@qq.com>
2022-06-22 10:02:47 +0800

    update

--- chaos/base/BlockingQueue.h
+++ chaos/base/BlockingQueue.h
@@ -3,5 +3,17 @@
 
 #include <assert.h>
 
+#include "nocopyable.h"
+
+/**
+ * 阻塞队列：在队列的基础上增加如下要求
+ * 1. 队列满时，队列会阻塞插入元素的线程，直到队列不满。
+ * 2. 队列空时，获取元素的线程会等待队列变为非空。
+ */
+namespace chaos
+{
+    
+} // namespace chaos
+
 
 #endif
--- chaos/base/Condition.h
+++ chaos/base/Condition.h
@@ -2,38 +2,62 @@
 #define CHAOS_CONDITION_H
 
 #include <pthread.h>
+#include <exception>
+
+#include "nocopyable.h"
 
 namespace chaos
 {
-    class Condition
+    class Condition : noncopyable
     {
     private:
         pthread_cond_t pcond_;
+        // pthread_mutex_t& mutex_;
         
     public:
-        Condition(/* args */);
+        Condition();
         
         ~Condition();
 
-        bool wait();
+        bool wait(pthread_mutex_t *mutex_);
 
-        bool timewait(double seconds);
+        bool timewait(pthread_mutex_t *mutex_, struct timespec t);
 
         // signal
-        void notify();
+        bool notify();
 
         // broadcast
-        void notifyAll();
+        bool notifyAll();
 
     };
     
-    Condition::Condition(/* args */)
+    Condition::Condition()
     {
+        if (pthread_cond_init(&pcond_, NULL) != 0)
+        {
+            throw std::exception();
+        }
     }
     
-    Condition::~Condition()
+    Condition::~Condition() { pthread_cond_destroy(&pcond_); }
+
+    bool Condition::wait(pthread_mutex_t *mutex_)
     {
+        int ret = 0;
+        ret = pthread_cond_wait(&pcond_, mutex_);
+        return ret == 0;
     }
+
+    bool Condition::timewait(pthread_mutex_t *mutex_, struct timespec t)
+    {
+        int ret = 0;
+        ret = pthread_cond_timedwait(&pcond_, mutex_, &t);
+        return ret == 0;
+    }
+
+    bool Condition::notify() { return pthread_cond_signal(&pcond_) == 0; }
+
+    bool Condition::notifyAll() { return pthread_cond_broadcast(&pcond_) == 0; }
 
 } // namespace chaos
 
